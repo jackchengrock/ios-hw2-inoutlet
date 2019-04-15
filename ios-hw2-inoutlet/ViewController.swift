db3e36499d07c69acef20d64caf4528821254d2b
jackchengrock <jackchengrock@GMAIL.COM>
2019-04-16 01:08:48 +0800

    Initial Commit

---
+++ ios-hw2-inoutlet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-hw2-inoutlet
+//
+//  Created by Jack Cheng on 2019/4/16.
+//  Copyright © 2019 jackrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
